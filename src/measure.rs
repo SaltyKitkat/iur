87d1257983d54baf27561def689cba8fbe4401e9
SaltyKitkat <954537646@qq.com>
2021-03-15 11:46:58 +0800

    add coretemp.rs

--- src/measure.rs
+++ src/measure.rs
@@ -14,14 +14,14 @@ pub fn sub_run(args: &ArgMatches) -> ! {
 
     let hwmon = coretemp::init();
     let mut powercap = powercap::init();
-    sleep(Duration::from_millis(5));
+    sleep(Duration::from_millis(5)); // prevent powercap from div 0 at the first calculate, may lead to inaccurate result in the first show
 
     print!("\x1b[H\x1b[J");
     loop {
         powercap::print(&mut powercap);
         println!("\x1b[K");
         coretemp::print(&hwmon);
-        println!("{}", delay_time);
+        // println!("{}", delay_time);
         print!("\x1b[J\x1b[H");
         sleep(Duration::from_secs_f64(delay_time));
     }
