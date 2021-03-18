9c259768206a56b2a51f493187a718efa8e29969
SaltyKitkat <954537646@qq.com>
2021-03-18 22:12:58 +0800

    rewrite something in powercap

--- src/modes/measure/mod.rs
+++ src/modes/measure/mod.rs
@@ -3,6 +3,7 @@ use std::{thread::sleep, time::Duration};
 use clap::ArgMatches;
 mod coretemp;
 mod powercap;
+use powercap::PowerCap;
 
 pub fn sub_run(args: &ArgMatches) -> ! {
     println!("measure mode");
@@ -13,7 +14,7 @@ pub fn sub_run(args: &ArgMatches) -> ! {
         .unwrap_or(2.0);
 
     let hwmon = coretemp::init();
-    let mut powercap = powercap::init();
+    let mut powercap = PowerCap::new();
     sleep(Duration::from_millis(5)); // prevent powercap from div 0 at the first calculate, may lead to inaccurate result in the first show
 
     print!("\x1b[H\x1b[J");
--- src/modes/measure/powercap.rs
+++ src/modes/measure/powercap.rs
@@ -27,52 +27,49 @@ impl PowerCap {
     pub fn len(&self) -> usize {
         self.0.len()
     }
-    fn new() -> PowerCap {
-        PowerCap(vec![], SystemTime::now())
-    }
-}
 
-pub fn init() -> PowerCap {
-    let mut powercap = PowerCap::new();
-    let tmp = Path::new("/sys/class/powercap");
-    let mut powercap_dir = tmp.read_dir().expect("Failed to open powercap directory");
-    if powercap_dir.all(|d| d.is_err()) {
-        panic!("Failed to open powercap directory");
-    }
-    for entry in powercap_dir {
-        match entry {
-            Ok(dir)
-                if {
-                    let tmp = dir.file_name();
-                    let tmp2 = tmp.to_string_lossy();
-                    tmp2.contains(":") && !tmp2.contains("mmio")
-                } =>
-            {
-                let mut name_path = dir.path();
-                name_path.push("name");
-                let name = if let Ok(mut name) = read_to_string(&name_path) {
-                    name.pop(); //remove the endding '\n'
-                    name
-                } else {
-                    eprintln!("failed to read from {}", name_path.to_string_lossy());
-                    continue;
-                };
-                let mut path = dir.path();
-                path.push("energy_uj");
-                powercap.0.push(PowerSensor::new(name, path));
-            }
-            _ => (),
+    pub fn new() -> PowerCap {
+        let mut powercap = PowerCap(vec![], SystemTime::now());
+        let tmp = Path::new("/sys/class/powercap");
+        let mut powercap_dir = tmp.read_dir().expect("Failed to open powercap directory");
+        if powercap_dir.all(|d| d.is_err()) {
+            panic!("Failed to open powercap directory");
         }
+        for entry in powercap_dir {
+            match entry {
+                Ok(dir)
+                    if {
+                        let tmp = dir.file_name();
+                        let tmp2 = tmp.to_string_lossy();
+                        tmp2.contains(":") && !tmp2.contains("mmio")
+                    } =>
+                {
+                    let mut name_path = dir.path();
+                    name_path.push("name");
+                    let name = if let Ok(mut name) = read_to_string(&name_path) {
+                        name.pop(); //remove the endding '\n'
+                        name
+                    } else {
+                        eprintln!("failed to read from {}", name_path.to_string_lossy());
+                        continue;
+                    };
+                    let mut path = dir.path();
+                    path.push("energy_uj");
+                    powercap.0.push(PowerSensor::new(name, path));
+                }
+                _ => (),
+            }
+        }
+        for i in &mut powercap.0 {
+            let tmp = match read_to_string(&i.path) {
+                Ok(s) => s,
+                Err(_) => continue,
+            };
+            let tmp = &tmp[0..tmp.len() - 1];
+            i.last = tmp.parse().unwrap_or_default();
+        }
+        powercap
     }
-    for i in &mut powercap.0 {
-        let tmp = match read_to_string(&i.path) {
-            Ok(s) => s,
-            Err(_) => continue,
-        };
-        let tmp = &tmp[0..tmp.len() - 1];
-        i.last = tmp.parse().unwrap_or_default();
-    }
-    powercap
 }
 
 pub fn print(powercap: &mut PowerCap) {
